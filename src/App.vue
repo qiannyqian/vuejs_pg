59e0bf13044348f039f19ae9ccc131aef41b923c
Hui Qian <huiqian@jobkred.com>
2018-10-15 10:34:29 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <my-counter></my-counter>
+  </div>
+</template>
+
+<script>
+import MyCounter from './components/MyCounter.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MyCounter
+  },
+
+  data () {
+    return {
+      count: 0,
+    }
+  },
+
+  created () {
+    window.vm = this
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <div class="counter">
+      {{count}}
+    </div>
+    <button v-on:click="count++">Click to +1</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0,
+    }
+  }
+}
+</script>
